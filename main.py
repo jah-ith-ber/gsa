612572bee5836e8ee1e04299be3e59721ae0677a
Mike Williams <micwill006@gmail.com>
2018-06-28 17:02:40 -0600

    added semaphore limit for cpu usage.

--- main.py
+++ main.py
@@ -26,8 +26,7 @@ productList = csv.reader(csvList, delimiter='|')
 outputfile = open('./final.csv', 'w', newline='')
 outputwriter = csv.writer(outputfile, delimiter=';')
 
-# Do not follow links that we've already followed.
-links = []
+semaphore = asyncio.BoundedSemaphore(20)
 
 # Tried using 2 separate async functions - one for initial, one for vendor links
 async def get_page(client, url):  
@@ -44,195 +43,192 @@ async def get_page(client, url):
 #             return await r.read()
 
 # Our main async function
-async def parse_gsa_webpage(product):
-    prodname = product[0]
-    prodUrls = product[1].split('`')
+async def parse_gsa_webpage(product, sem):
+    async with sem: 
+        prodname = product[0]
+        prodUrls = product[1].split('`')
 
-    print('started ', prodname)
+        print('started ', prodname)
 
-    # List to prepare our csv rows for a csv writerow.
-    csv_obj = [[], [], []]
+        # List to prepare our csv rows for a csv writerow.
+        csv_obj = [[], [], []]
 
-    if 'No results found' not in prodUrls[0]:
-        # Loop through product links
-        for link in prodUrls:
-            # If we haven't visited this link
-            if link not in links:
-                print('initial request made to:')
+        if 'No results found' not in prodUrls[0]:
+            # Loop through product links
+            for link in prodUrls:
+                # If we haven't visited this link
+                    print('initial request made to:')
+                    # Grab the page asyncronously
+                    html_to_parse = await get_page(client, link)
+                    if html_to_parse == False:
+                        return
+                    print(link)
+                    # parse it
+                    soup = BeautifulSoup(html_to_parse, 'lxml')
+                else: #Otherwise skip it
+                    continue
 
-                # Grab the page asyncronously
-                html_to_parse = await get_page(client, link)
-                if html_to_parse == False:
-                    return
-                print(link)
-                # Throw the link in links
-                links.append(link)
-                # parse it
-                soup = BeautifulSoup(html_to_parse, 'lxml')
-            else: #Otherwise skip it
-                continue
+                # Find our elements to scrape from the page. Typically arent really any classes/id's so we have to navigate by attributes.
+                pricingTable = soup.findAll(attrs={
+                    'width': "100%",
+                    'border': "0",
+                    'cellpadding': "0",
+                    'cellspacing': "0",
+                    'class': 'greybox'
+                })[0]
 
-            # Find our elements to scrape from the page. Typically arent really any classes/id's so we have to navigate by attributes.
-            pricingTable = soup.findAll(attrs={
-                'width': "100%",
-                'border': "0",
-                'cellpadding': "0",
-                'cellspacing': "0",
-                'class': 'greybox'
-            })[0]
+                nameTable = soup.findAll(attrs={
+                    'width': "100%",
+                    'border': "0",
+                    'cellspacing': "0",
+                    'cellpadding': "0",
+                    'class': "black8pt"
+                })
 
-            nameTable = soup.findAll(attrs={
-                'width': "100%",
-                'border': "0",
-                'cellspacing': "0",
-                'cellpadding': "0",
-                'class': "black8pt"
-            })
-
-            moreSpecs = soup.findAll(attrs={
-                'width': "100%",
-                'border': "0",
-                'cellspacing': "0",
-                'cellpadding': "1",
-                'class': "black8pt"
-            })
-            
-            specsTab = ''
-            additionalDesc = ''
-
-            # Yay.. an id for once.
-            descTable = soup.findAll(id='TabbedPanels1')
-
-            # if a description table exists, grab it.
-            if len(descTable) > 0:
-                if len(descTable[0].select('.comment')) > 0:
-                    additionalDesc = descTable[0].select('.comment')
-                tabPanelContent = descTable[0].select(".TabbedPanelsContent table")
-
-            # If spec box exists
-            try:
-                speclist = tabPanelContent[1].findAll('tr')
-            except:
-                speclist = []
-
-            # Loop through the spec list, arrange it into a readable format for the csv.
-            try:
-                for spec in speclist:
-                    specs = spec.findAll('td')
-                    if len(specs) > 1:
-                        for i, v in enumerate(specs):
-                            # If we're at at a 'key' in the spec list
-                            if i == 1:
-                                specsTab += v.get_text() + '. '
-                            # If we're at at a 'value' in the spec list
-                            else:
-                                specsTab += v.get_text() + ': '
-            except:
-                pass
-
-            # Product Attributes
-            productName = nameTable[1].strong.string.replace(';', ' ')
-
-            trs = pricingTable.findAll('tr')
-            # Pricing table madness begins
-            for i, row in enumerate(trs):
-
-                row_tds = row.select('td')
-                row_ths = row.select('th')
-
-                ourList = []
-                our2List = []
-
-                vendorList = []
-                vendorIndex = []
-                count = 0
-
+                moreSpecs = soup.findAll(attrs={
+                    'width': "100%",
+                    'border': "0",
+                    'cellspacing': "0",
+                    'cellpadding': "1",
+                    'class': "black8pt"
+                })
                 
-                for e, v in enumerate(row_tds):
-                    print(e, ' - td iteration')
-                    if i == 0:
-                        if len(ourList) > 0 and e > 1:
-                            if v.get_text().strip() == ourList[-1]:
+                specsTab = ''
+                additionalDesc = ''
+
+                # Yay.. an id for once.
+                descTable = soup.findAll(id='TabbedPanels1')
+
+                # if a description table exists, grab it.
+                if len(descTable) > 0:
+                    if len(descTable[0].select('.comment')) > 0:
+                        additionalDesc = descTable[0].select('.comment')
+                    tabPanelContent = descTable[0].select(".TabbedPanelsContent table")
+
+                # If spec box exists
+                try:
+                    speclist = tabPanelContent[1].findAll('tr')
+                except:
+                    speclist = []
+
+                # Loop through the spec list, arrange it into a readable format for the csv.
+                try:
+                    for spec in speclist:
+                        specs = spec.findAll('td')
+                        if len(specs) > 1:
+                            for i, v in enumerate(specs):
+                                # If we're at at a 'key' in the spec list
+                                if i == 1:
+                                    specsTab += v.get_text() + '. '
+                                # If we're at at a 'value' in the spec list
+                                else:
+                                    specsTab += v.get_text() + ': '
+                except:
+                    pass
+
+                # Product Attributes
+                productName = nameTable[1].strong.string.replace(';', ' ')
+
+                trs = pricingTable.findAll('tr')
+                # Pricing table madness begins
+                for i, row in enumerate(trs):
+
+                    row_tds = row.select('td')
+                    row_ths = row.select('th')
+
+                    ourList = []
+                    our2List = []
+
+                    vendorList = []
+                    vendorIndex = []
+                    count = 0
+
+                    
+                    for e, v in enumerate(row_tds):
+                        print(e, ' - td iteration')
+                        if i == 0:
+                            if len(ourList) > 0 and e > 1:
+                                if v.get_text().strip() == ourList[-1]:
+                                    break
+                            if v.get_text().strip() == 'Price/Unit':
+                                ourList.append('Price')
+                                ourList.append('')
+                                ourList.append('Unit')
+                            elif v.get_text().strip() == 'Features':
+                                ourList.append('Features')
+                                ourList.append('')
                                 break
-                        if v.get_text().strip() == 'Price/Unit':
-                            ourList.append('Price')
-                            ourList.append('')
-                            ourList.append('Unit')
-                        elif v.get_text().strip() == 'Features':
-                            ourList.append('Features')
-                            ourList.append('')
-                            break
+                            else:
+                                ourList.append(''.join(e for e in v.get_text().replace('\n', ' ').strip() if e.isalnum() or e == '.' or e == '$' )) 
+                        elif v.img != None:
+                            if v.img['alt'].strip() != '':
+                                ourList.append(v.img['alt'].strip())
                         else:
-                            ourList.append(''.join(e for e in v.get_text().replace('\n', ' ').strip() if e.isalnum() or e == '.' or e == '$' )) 
-                    elif v.img != None:
-                        if v.img['alt'].strip() != '':
-                            ourList.append(v.img['alt'].strip())
-                    else:
-                        if '&nbsp;' not in v.get_text().strip():
-                            ourList.append(''.join(e for e in v.get_text().replace('\n', ' ').strip() if e.isalnum() or e == '.' or e == '$' )) 
-                        # if we find links in this tr and it contains catalog (vendor link)
-                        if len(v.select('font[size="2"] a')) > 0:
-                            if 'catalog' in v.select('font[size="2"] a')[0].get('href'):
-                                newlink = 'https://www.gsaadvantage.gov' + v.select('font[size="2"] a')[
-                                    0].get('href')
-                for e, v in enumerate(row_ths):
-                    print(e, ' - theaders iteration')
+                            if '&nbsp;' not in v.get_text().strip():
+                                ourList.append(''.join(e for e in v.get_text().replace('\n', ' ').strip() if e.isalnum() or e == '.' or e == '$' )) 
+                            # if we find links in this tr and it contains catalog (vendor link)
+                            if len(v.select('font[size="2"] a')) > 0:
+                                if 'catalog' in v.select('font[size="2"] a')[0].get('href'):
+                                    newlink = 'https://www.gsaadvantage.gov' + v.select('font[size="2"] a')[
+                                        0].get('href')
+                    for e, v in enumerate(row_ths):
+                        print(e, ' - theaders iteration')
+                        if i == 0:
+                            if v.get_text().strip() == 'Contractor':
+                                count = e
+                            our2List.append(''.join(e for e in v.get_text().replace('\n', ' ').strip() if e.isalnum() or e == '.' or e == '$' ))                                 
+                            our2List.append('')
+
+                    ourList[0] = prodname
+
                     if i == 0:
-                        if v.get_text().strip() == 'Contractor':
-                            count = e
-                        our2List.append(''.join(e for e in v.get_text().replace('\n', ' ').strip() if e.isalnum() or e == '.' or e == '$' ))                                 
-                        our2List.append('')
-
-                ourList[0] = prodname
-
-                if i == 0:
-                    if type(additionalDesc) == list:
-                        if type(additionalDesc[0]) != str:
-                            csv_obj[0] = 'prodNum;productName;prodUrl;specsTab;additionalDesc'.split(';')
+                        if type(additionalDesc) == list:
+                            if type(additionalDesc[0]) != str:
+                                csv_obj[0] = 'prodNum;productName;prodUrl;specsTab;additionalDesc'.split(';')
+                                csv_obj[1].append(prodname)
+                                csv_obj[1].append(productName)
+                                csv_obj[1].append(link)
+                                csv_obj[1].append(specsTab)
+                                csv_obj[1].append(additionalDesc[0].get_text().strip())
+                        else:
+                            csv_obj[0] = 'prodNum;productName;prodUrl;specsTab'.split(';')                     
                             csv_obj[1].append(prodname)
                             csv_obj[1].append(productName)
                             csv_obj[1].append(link)
                             csv_obj[1].append(specsTab)
-                            csv_obj[1].append(additionalDesc[0].get_text().strip())
+
+
+                    if len(ourList) > 0:
+                        csv_obj[2].append(ourList + our2List)
                     else:
-                        csv_obj[0] = 'prodNum;productName;prodUrl;specsTab'.split(';')                     
-                        csv_obj[1].append(prodname)
-                        csv_obj[1].append(productName)
-                        csv_obj[1].append(link)
-                        csv_obj[1].append(specsTab)
+                        continue
 
+                outputwriter.writerow(csv_obj[0])
+                csv_obj[0] = []
+                outputwriter.writerow(csv_obj[1])
+                csv_obj[1] = []
+                for i, item in enumerate(csv_obj[2]):
+                    if item[2] != '':
+                        outputwriter.writerow(item)
+                csv_obj[2] = []
+                print('writerow')
 
-                if len(ourList) > 0:
-                    csv_obj[2].append(ourList + our2List)
-                else:
-                    continue
+        global our_count
+        global csv_len
 
-            outputwriter.writerow(csv_obj[0])
-            csv_obj[0] = []
-            outputwriter.writerow(csv_obj[1])
-            csv_obj[1] = []
-            for i, item in enumerate(csv_obj[2]):
-                if item[2] != '':
-                    outputwriter.writerow(item)
-            csv_obj[2] = []
-            print('writerow')
+        our_count += 1
 
-    global our_count
-    global csv_len
+        print('our_count = ', our_count)
+        print('csv_len = ', csv_len)
 
-    our_count += 1
-
-    print('our_count = ', our_count)
-    print('csv_len = ', csv_len)
-
-    if our_count == csv_len:
-        loop.stop()
-        await client.close()
-        print('All tasks completed.')
+        if our_count == csv_len:
+            loop.stop()
+            await client.close()
+            print('All tasks completed.')
 
 for i, item in enumerate(productList):
     csv_len += 1
-    asyncio.ensure_future(parse_gsa_webpage(item), loop = loop)
+    asyncio.ensure_future(parse_gsa_webpage(item, semaphore), loop = loop)
 try:
     loop.run_forever()
 finally:
